29a7f83637b40149de2ebf89c8785e204e8cb73b
Nevaan Perera <nevaan9@gmail.com>
2019-02-02 01:59:36 -0500

    Git Init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
