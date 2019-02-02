8f9fdf03428f1229ad78e4d40628a91c358aa1db
Nevaan Perera <nevaan9@gmail.com>
2019-02-02 12:21:55 -0500

    Added NavBar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
+    <Header></Header>
     <router-view></router-view>
     <Footer></Footer>
   </v-app>
 </template>
 
 <script>
+import Header from './components/Header'
 import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
+    Header,
     Footer
   },
   data() {
-    return {
-      //
-    }
+    return {}
   }
 }
 </script>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout wrap style="height: 100px;">
+    <v-container class="pa-0 ma-0">
+      <v-layout>
+        <v-btn dark @click.stop="drawer = !drawer">Toggle</v-btn>
+      </v-layout>
+    </v-container>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
