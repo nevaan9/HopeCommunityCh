100f4ee5120264a3075e4919aaaa1e764914e69f
Nevaan Perera <nevaan9@gmail.com>
2019-02-05 19:09:47 -0500

    Added Signup Page

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import '@mdi/font/css/materialdesignicons.css'
-import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
   theme: {
-    primary: colors.black,
-    secondary: colors.grey.darken1,
-    accent: colors.shades.black,
-    error: colors.red.accent3
+    primary: '#1976D2',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
   }
 })
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,9 +1,73 @@
 <template>
-  <div>SignUP</div>
+  <v-content>
+    <v-layout column wrap align-center justify-center fill-height>
+      <v-card>
+        <v-toolbar dark>
+          <span class="title">Sign Up</span>
+        </v-toolbar>
+        <v-card-text>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-text-field
+                label="Name"
+                single-line
+                outline
+                v-model="name"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                label="Email"
+                single-line
+                outline
+                v-model="email"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                label="Password"
+                single-line
+                outline
+                v-model="password"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                label="Confirm Password"
+                single-line
+                outline
+                v-model="confirmPassword"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="green">Sign Up</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card class="text-xs-center elevation-0 transparent">
+        <v-card-text>
+          <router-link tag="p" :to="{ name: 'login' }">
+            <a>Back to login page</a>
+          </router-link>
+        </v-card-text>
+      </v-card>
+    </v-layout>
+  </v-content>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+}
 </script>
 
 <style scoped></style>
