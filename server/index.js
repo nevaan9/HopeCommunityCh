d6bbe20736c9c122b3a99007bbe361ddcecf42ac
Nevaan Perera <nevaan9@gmail.com>
2019-03-29 01:23:34 -0400

    cwip

--- server/index.js
+++ server/index.js
@@ -1,12 +1,62 @@
 // Load the global configs
 require("./config/config");
+// Connect to the DB
+// require("./db/connectDB");
 const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const cors = require("cors");
 const app = express();
-const { Home } = require("./models/Home");
 const { removeImage } = require("./utils/utils");
+const mongoose = require("mongoose");
+const { Home } = require("./models/Home");
+// Enable mongoose to user promises
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    Home.find({}).then(homeObj => {
+      if (!homeObj.length) {
+        new Home({
+          heading: "New Home Community Church From DB!",
+          subHeading: "We are live from the DB!",
+          mainButtonText: "Learn more about us!",
+          mainCoverPicture: "hello.png",
+          mainCenterPicture: "hello.png",
+          churchesHeader: "Our Churches from the DB!",
+          churchesSubHeader: "Lets goooo",
+          churchOneInfo: {
+            picture: "hello.png",
+            text: "Church #1",
+            location: "Somewhere nice",
+            time: "12 midnight!",
+            description: "My heart goes shalalala"
+          },
+          churchTwoInfo: {
+            picture: "hello.png",
+            text: "Churchy #2",
+            location: "Somewhere nicer",
+            time: "12 noon!",
+            description: "Yo yo yo yoy yo"
+          },
+          secondaryCoverPicture: "hello.png",
+          churchInfoSectionOne: {
+            title: "Info section #1",
+            description: "des #1"
+          },
+          churchInfoSectionTwo: {
+            title: "info #2",
+            description: "desc #2"
+          }
+        })
+          .save()
+          .then(() => {})
+          .catch(e => {
+            throw new Error(e);
+          });
+      }
+    });
+  });
 
 // Configure how multer stores files
 const fileStorage = multer.diskStorage({
@@ -43,9 +93,6 @@ const upload = multer({ storage: fileStorage, fileFilter: fileFilter }).single(
   "image"
 );
 app.use(cors());
-
-// Connect to the DB
-require("./db/connectDB");
 
 // Serve the images
 app.use(express.static(__dirname + "/images/"));
